3c47f22ee79314da062fc3650e2f782af3ec34d4
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2020-08-30 22:18:35 +0300

    style: remove unused styling scss file

--- src/client/index.js
+++ src/client/index.js
@@ -14,7 +14,6 @@ import './styles/defaults.scss'
 import './styles/header.scss'
 import './styles/base.scss'
 import './styles/slide-show.scss'
-import './styles/form.scss'
 import './styles/decorators.scss'
 import './styles/footer.scss'
 

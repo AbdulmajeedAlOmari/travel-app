a897ff87ae5a1cb72e07078cd2d74767961f4974
Abdulmajeed Alomari <abdulmajeedmmo@gmail.com>
2020-08-30 21:35:34 +0300

    refactor(server.js): seperate server from configurations to be able to use it in unit testing

---
+++ src/server/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+/* -----[ Dependencies ]----- */
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+
+
+/* -----[ Middlewares ]----- */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('tiny'))
+app.use(express.static('dist'))
+
+
+/* -----[ Required Routes ]----- */
+const travelRoutes = require('./routes/travel')
+
+
+/* -----[ Routing ]----- */
+// Index Route
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// Travel related routes
+app.use('/travel', travelRoutes)
+
+module.exports = app
